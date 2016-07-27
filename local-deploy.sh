76843de52ba18e8e803df46b7486e2186ae453f2
Jerry Chanut <ext.steria.jerry.chanut@sncf.fr>
2016-07-27 15:56:25 +0200

    Improving local-deploy.sh

--- local-deploy.sh
+++ local-deploy.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-
+echo 'Destroy previously created Vagrant vm'
 vagrant destroy -f
+
+echo
+echo 'Remove old pmm boxes from Vagrant'
 vagrant box remove ddsim-pmm/ubuntu-12.04.4
 vagrant box remove ddsim-pmm/ubuntu-14.04.2
+
+echo
+echo 'Adding new pmm boxes to Vagrant'
 vagrant box add pmm-ubuntu-12.04.4-vagrant.json
 vagrant box add pmm-ubuntu-14.04.2-vagrant.json
+
+echo
+echo 'Starting Vagrant vm'
 vagrant up
